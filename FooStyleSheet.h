89adf0eb4f721ce86c9d1b3c1dc88eb8a2db660d
Alper Tayfun <alper.tayfun@hotmail.com>
2011-04-11 14:49:10 +0300

    new git new app

---
+++ FooStyleSheet.h
@@ -0,0 +1,17 @@
+//
+//  FooStyleSheet.h
+//  beatiful girls
+//
+//  Created by Alper Tayfun on 03-24-2011.
+//  Copyright 2011 Alper Tayfun. All rights reserved.
+//
+
+#import <Three20Style/Three20Style.h> 
+#import <Three20Style/TTStyleSheet.h> 
+#import <Three20Style/TTDefaultStyleSheet.h> 
+
+@interface FooStyleSheet : TTDefaultStyleSheet 
+
+@property (nonatomic, readonly) UIColor* toolbarTintColor; 
+
+@end
---
+++ FooStyleSheet.m
@@ -0,0 +1,17 @@
+//
+//  FooStyleSheet.m
+//  beatiful girls
+//
+//  Created by Alper Tayfun on 03-24-2011.
+//  Copyright 2011 Alper Tayfun. All rights reserved.
+//
+
+#import "FooStyleSheet.h" 
+
+@implementation FooStyleSheet 
+
+- (UIColor*)toolbarTintColor { 
+    return RGBCOLOR(0, 0, 0); 
+}
+
+@end
